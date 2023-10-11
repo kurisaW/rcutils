2eb8c59a731251022fea37304691c8ec9988b997
kurisaw <2053731441@qq.com>
2023-10-10 20:43:58 -0700

    [modify]

--- src/time_unix.c
+++ src/time_unix.c
@@ -36,7 +36,7 @@ extern "C"
 #else
 #include <time.h>
 #endif // ZEPHYR_VERSION_CODE >= ZEPHYR_VERSION(3, 1, 0)
-#elif defined(_POSIX_MONOTONIC_CLOCK) //__rtems__
+#elif defined(__rtems__) //__rtems__
 #include <time.h>
 #else
 #include <time.h>
